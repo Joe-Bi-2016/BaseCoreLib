0bfe106665c09dd3525784b6d5ff51eb0e3dfbfb
Joe-Bi-2016 <xinkong886@163.com>
2026-07-04 00:04:35 +0800

    optimize SimpleFixedThreadpool and modify bug

--- BaseCoreLib/inc/os/SimpleFixedThreadpool.hpp
+++ BaseCoreLib/inc/os/SimpleFixedThreadpool.hpp
@@ -19,6 +19,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <exception>
+#include <chrono>
+#include <string>
 
 //---------------------------------------------------------------------------//
 __BEGIN__
@@ -26,45 +28,82 @@ __BEGIN__
 	//-----------------------------------------------------------------------//
 	class fixed_thread_pool {
 	public:
-		explicit fixed_thread_pool(size_t thread_count)
-			: data_(std::make_shared<data>()) {
-			threads_.reserve(thread_count);		
-			for (size_t i = 0; i < thread_count; ++i) {
-				threads_.emplace_back([data = data_] {
-                while (true) {
-                    std::function<void()> task;
-                    {
-                        std::unique_lock<std::mutex> lk(data->mtx_);
-                        data->cond_.wait(lk, [&] { return data->is_shutdown_ || !data->tasks_.empty();});
-                        if (data->is_shutdown_ && data->tasks_.empty())
-                            break;
-                        task = std::move(data->tasks_.front());
-                        data->tasks_.pop();
-                    }
-                    try {
-                        task();
-                    } catch (...) {
-						std::string msg = "Unknown exception";
-                        try {
-                            std::rethrow_exception(std::current_exception());
-                        } catch (const std::exception& e) {
-                            msg = e.what();
-                        } catch (...){ }
-                        
-						std::function<void(const char*)> handler;
-						{
-							std::lock_guard<std::mutex> lk(data_->mtx_);
-							handler = data_->on_exception;
-						}
+		explicit fixed_thread_pool(size_t thread_count = 10, size_t max_queue_size = 0)
+        : data_(std::make_shared<data>(max_queue_size)),
+          thread_count_(thread_count){
+			if (thread_count == 0)
+				throw std::invalid_argument("thread_count must be > 0");
+  
+			threads_.reserve(thread_count);	
 
-                        if (handler) {
-                            handler(msg.c_str());
-                        } else {
-                            std::cerr << "Exception in thread pool: " << msg << '\n';
-                        }
-                    }
-                }
-            });
+			try {
+				for (size_t i = 0; i < thread_count; ++i) {
+					threads_.emplace_back([data = data_] {
+					while (true) {
+						std::function<void()> task;
+						{
+							std::unique_lock<std::mutex> lk(data->mtx_);
+							data->cond_.wait(lk, [&] { return data->is_shutdown_ || !data->tasks_.empty();});
+							if (data->is_shutdown_ && data->tasks_.empty())
+								break;
+							task = std::move(data->tasks_.front());
+							data->tasks_.pop();
+							
+							if (data->max_queue_size_ > 0)
+                                data->full_cond_.notify_one();
+						}
+						try {
+							task();
+						} catch (...) {
+							std::string msg = "Unknown exception";
+							try {
+								std::rethrow_exception(std::current_exception());
+							} catch (const std::exception& e) {
+								msg = e.what();
+							} catch (...){ }
+							
+							std::function<void(const char*)> handler;
+							{
+								std::lock_guard<std::mutex> lk(data_->mtx_);
+								handler = data_->on_exception;
+							}
+	
+							if (handler) {
+                                try {
+                                    handler(msg.c_str());
+                                } catch (...) {
+									
+                                }
+							} else {
+								std::cerr << "Exception in thread pool: " << msg << '\n';
+							}
+						}
+					}
+					});
+				}
+			} catch(...) {
+				{
+					std::lock_guard<std::mutex> lk(data_->mtx_);
+					data_->is_shutdown_ = true;
+				}
+				data_->cond_.notify_all();
+				
+				if (data_->max_queue_size_ > 0)
+					data_->full_cond_.notify_all();
+					
+				for (auto& t : threads_) {
+					if (t.joinable()) {
+						try {
+							t.join();
+						} catch (...) {
+							try {
+								t.detach();
+							} catch (...) {}
+						}
+					}
+				}
+				
+				throw;
 			}
 		}
 	
@@ -74,14 +113,18 @@ __BEGIN__
 	
 		fixed_thread_pool(fixed_thread_pool&& other) noexcept
         : data_(std::move(other.data_)),
-          threads_(std::move(other.threads_)) { 
-		}
-		
+          threads_(std::move(other.threads_)),
+          thread_count_(other.thread_count_) {
+			  other.thread_count_ = 0;
+		  }
+
 		fixed_thread_pool& operator=(fixed_thread_pool&& other) noexcept {
 			if (this != &other) {
 				shutdown_and_join();
 				data_ = std::move(other.data_);
 				threads_ = std::move(other.threads_);
+				thread_count_ = other.thread_count_;
+				other.thread_count_ = 0;
 			}
 			return *this;
 		}
@@ -100,40 +143,128 @@ __BEGIN__
 		explicit operator bool() const noexcept {
 			return data_ != nullptr && !threads_.empty();
 		}
+		
+		size_t thread_count() const noexcept {
+			return thread_count_;
+		}
+
+		size_t queue_size() const {
+			if (!data_) return 0;
+			std::lock_guard<std::mutex> lk(data_->mtx_);
+			return data_->tasks_.size();
+		}
+	
+		bool is_shutdown() const {
+			if (!data_) return true;
+			std::lock_guard<std::mutex> lk(data_->mtx_);
+			return data_->is_shutdown_;
+		}
 	
 		template <class F>
 		void execute(F&& task, void* arg) {
 			if (!data_)
 				throw std::runtime_error("fixed_thread_pool: execute called on moved-from object");
+			
+			std::function<void()> wrapped = [task = std::forward<F>(task), arg] { task(arg); };
+			
 			{
-				std::lock_guard<std::mutex> lk(data_->mtx_);
-				data_->tasks_.emplace([task = std::forward<F>(task), arg] { task(arg); });
+				std::unique_lock<std::mutex> lk(data_->mtx_);
+
+				if (data_->is_shutdown_)
+					throw std::runtime_error("fixed_thread_pool: pool is shut down");
+
+				// block waiting
+				if (data_->max_queue_size_ > 0) {
+					data_->full_cond_.wait(lk, [&] {
+						return data_->is_shutdown_ || data_->tasks_.size() < data_->max_queue_size_;
+					});
+					if (data_->is_shutdown_) {
+						throw std::runtime_error("fixed_thread_pool: pool shut down during enqueue");
+					}
+				}
+
+				// Optimization: Only notify when the queue is empty to avoid unnecessary wake-ups
+				bool was_empty = data_->tasks_.empty();
+				data_->tasks_.push(std::move(wrapped));
+				if (was_empty)
+					data_->cond_.notify_one();
 			}
-			data_->cond_.notify_one();
 		}
-	
-	private:
-		struct data {
-			std::mutex mtx_;
-			std::condition_variable cond_;
-			bool is_shutdown_ = false;
-			std::queue<std::function<void()>> tasks_;
-			std::function<void(const char*)> on_exception;
-		};
-		std::shared_ptr<data> data_;
-		std::vector<std::thread> threads_;
 		
-		void shutdown_and_join() {
+		template<class F>
+		bool execute_for(F&& task, void* arg, std::chrono::milliseconds timeout) {
+			if (!data_)
+				return false;
+	
+			std::function<void()> wrapped = [task = std::forward<F>(task), arg] {
+				task(arg);
+			};
+	
+			{
+				std::unique_lock<std::mutex> lk(data_->mtx_);
+	
+				if (data_->is_shutdown_)
+					return false;
+	
+				if (data_->max_queue_size_ > 0) {
+					bool success = data_->full_cond_.wait_for(lk, timeout, [&] {
+						return data_->is_shutdown_ ||
+							data_->tasks_.size() < data_->max_queue_size_;
+					});
+					if (!success || data_->is_shutdown_)
+						return false;
+				}
+	
+				// Optimization: Only notify when the queue is empty to avoid unnecessary wake-ups
+				bool was_empty = data_->tasks_.empty();
+				data_->tasks_.push(std::move(wrapped));
+				if (was_empty)
+					data_->cond_.notify_one();
+			}
+			return true;
+		}
+		
+		void shutdown() {
 			if (data_) {
 				{
 					std::lock_guard<std::mutex> lk(data_->mtx_);
 					data_->is_shutdown_ = true;
 				}
 				data_->cond_.notify_all();
+				if (data_->max_queue_size_ > 0)
+					data_->full_cond_.notify_all();
 			}
-			
+		}
+	
+	private:
+		struct data {
+			explicit data(size_t max_q) : max_queue_size_(max_q) {}
+			std::mutex mtx_;
+			std::condition_variable cond_;
+			std::condition_variable full_cond_;
+			bool is_shutdown_ = false;
+			std::queue<std::function<void()>> tasks_;
+			size_t max_queue_size_;
+			std::function<void(const char*)> on_exception;
+		};
+		
+		std::shared_ptr<data> data_;
+		std::vector<std::thread> threads_;
+		size_t thread_count_ = 0;
+		
+		void shutdown_and_join() noexcept {
+			shutdown();			
 			for (auto& t : threads_) {
-				if (t.joinable()) t.join();
+				if (t.joinable()) {
+					try {
+						t.join();
+					} catch (...) {
+						try {
+							t.detach();
+						} catch (...) {
+						}
+					}
+				}
 			}
 		}
 	};
@@ -159,7 +290,7 @@ __BEGIN__
 	}
 	
 	//example:
-	//fixed_thread_pool pool(10);
+	//fixed_thread_pool pool(10, 100);
 	// pool.set_exception_handler([](const char* msg) {
 	//     std::cerr << "Custom handler: " << msg << '\n';
 	// });
